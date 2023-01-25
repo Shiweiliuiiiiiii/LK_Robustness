e3e900ded5fe45466e8e38393c6b33cab0ba37a6
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2023-01-25 15:58:57 -0600

    fix distortion import issue

--- scripts_ImageNet_C/Imagenet_C_rn501.sh
+++ scripts_ImageNet_C/Imagenet_C_rn501.sh
@@ -6,11 +6,11 @@ data_type=imagenet-c
 
 #distill from vit
 ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_VitTeacher_STRN50_NKD/checkpoint-best.pth
-python main.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
+python main_robust.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
 
 #distill from swin
 ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_SwinTeacher_STRN50_NKD/checkpoint-best.pth
-python main.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
+python main_robust.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
 
 
 
--- scripts_ImageNet_C/Imagenet_C_rn502.sh
+++ scripts_ImageNet_C/Imagenet_C_rn502.sh
@@ -7,15 +7,15 @@ data_type=imagenet-c
 
 #distill from cswin
 ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_TeachersCSwin_STRN50_NKD/checkpoint-best.pth
-python main.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
+python main_robust.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
 
 #distill from convNeXt
 ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_ConvNeXtTeacher_STRN50_NKD/checkpoint-best.pth
-python main.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
+python main_robust.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
 
 #distill from SLaK-T
 ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_SLaKTTeachers_STRN50_NKD/checkpoint-best.pth
-python main.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
+python main_robust.py --model resnet50 --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn False --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
 
 
 
