7d0e0da4926a436eb515ed75c223071c07122b62
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2023-01-25 15:43:04 -0600

    fix distortion import issue

--- scripts_ImageNet_C/Imagenet_C_convnext1.sh
+++ scripts_ImageNet_C/Imagenet_C_convnext1.sh
@@ -6,7 +6,7 @@ data_type=imagenet-c
 
 #student: convnext-T
 #original convnext-T 120ep
-ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/Student/convnext_120ep/checkpoint-best.pthcd
+ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/Student/convnext_120ep/checkpoint-best.pth
 python main_robust.py --model SLaK_tiny --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 \
 --bn True --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data  \
 --output_dir /workspace/ssd1/shiwei/LK_distill/Table_1_CKS/Student/convnext_120ep/
