028d59de088aca238ec503084073cbbd282b4926
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2023-01-26 00:35:20 -0600

    test convnext from slak

--- scripts_ImageNet_C/Imagenet_C_convnext2.sh
+++ scripts_ImageNet_C/Imagenet_C_convnext2.sh
@@ -17,7 +17,7 @@ data_type=imagenet-c
 #python main_robust.py --model SLaK_tiny --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
 
 #distill from SLakT
-ck1=/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_SLaKTTeacher_STConvNext_NKD/checkpoint-best.pth
+ck1=/workspace/ssd1/shiwei/LK_distill/Table_1_CKS/bnTrue_SLaKTTeacher_STConvNext_NKD/checkpoint-best.pth
 python main_robust.py --model SLaK_tiny --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume $ck1 --input_size 224 --data_type $data_type --drop_path 0.1 --data_path $data
 
 
