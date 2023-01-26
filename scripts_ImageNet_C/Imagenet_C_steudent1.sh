b93c03c205bb7b8149ead272e99914b2e119dc95
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2023-01-25 19:21:26 -0600

    FIX swin and vits

--- scripts_ImageNet_C/Imagenet_C_steudent1.sh
+++ scripts_ImageNet_C/Imagenet_C_steudent1.sh
@@ -4,10 +4,10 @@ data_type=imagenet-c
 
 ####### test teacher models #####################
 #swin
-python main_robust.py --model swin --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
+python main_robust.py --model swin --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume '' --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
 
 #vit-s
-python main_robust.py --model vit --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
+python main_robust.py --model vit --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume '' --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
 
 
 
