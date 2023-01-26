ab8c04056b7507729bb67f7679aed68d222ef356
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2023-01-25 19:17:41 -0600

    add RN50

--- scripts_ImageNet_C/Imagenet_C_steudent1.sh
+++ scripts_ImageNet_C/Imagenet_C_steudent1.sh
@@ -1,7 +1,7 @@
 data=/workspace/ssd1/shiwei/data/Imagenet-robustness/Imagenet-c #改下路径
 data_type=imagenet-c
 
-cd ..
+
 ####### test teacher models #####################
 #swin
 python main_robust.py --model swin --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 --bn True --Decom False  --resume $ck1 --data_type $data_type --input_size 224 --drop_path 0.1 --data_path $data
