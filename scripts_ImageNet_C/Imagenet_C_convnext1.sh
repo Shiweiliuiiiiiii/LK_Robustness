87823baeca782c50baf11688dca0a59f306120a7
Shiwei Liu <shiwei@utcsadmins-MacBook-Pro.local>
2023-03-17 17:21:31 -0500

    test 300 convnext

--- scripts_ImageNet_C/Imagenet_C_convnext1.sh
+++ scripts_ImageNet_C/Imagenet_C_convnext1.sh
@@ -3,10 +3,6 @@ data_type=imagenet-c
 
 
 
-export PATH='/usr/local/cuda-11.4/bin:$PATH'
-export LD_LIBRARY_PATH='/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH'
-export CUDA_PATH='/usr/local/cuda-11.4'
-
 ck1=/workspace/ssd1/shiwei/LK_distill/slak_Convnext_t_300_epoch/checkpoint-best.pth
 CUDA_VISIBLE_DEVICES=7 python main_robust.py --model SLaK_tiny --eval True --kernel_size 7 7 7 7 100 --width_factor 1.0 \
 --bn True --Decom False  --resume /workspace/ssd1/shiwei/LK_distill/slak_Convnext_t_300_epoch/checkpoint-best.pth --data_type imagenet-c --input_size 224 --drop_path 0.1 --data_path /workspace/ssd1/shiwei/data/Imagenet-robustness/Imagenet-c  \
